eeaf1e832ad3f09ed94a0425cc0687181bca40a0
Dominik Manowski <manowski@protonmail.com>
2017-12-17 13:04:30 +0100

    fixing broken scrolltop button

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 //https://www.youtube.com/watch?v=x0YnVwAuNQI
 
 $(document).ready(function() {
-    var navLinks = $('.scroll');
+    var $navLinks = $('.scroll');
     var $navbar = $('#navbarSupportedContent');
     var $navbarBtn = $('.navbar-toggler');
 
     function scrollToSection(event) {
         event.preventDefault();
-        $navbar.toggleClass('show');
-        $navbarBtn.toggleClass('collapsed');
+        var hideMenu = $(event.delegateTarget).data('hide-menu');
+
+        if (hideMenu === 'yes') {
+            $navbar.removeClass('show');
+        } else {
+            $navbarBtn.toggleClass('collapsed');
+            $navbar.toggleClass('show');
+        }
+
         $('body, html').animate({
             scrollTop: $(this.hash).offset().top
-        }, 800 );
+        }, 800);
     }
-    navLinks.on('click', scrollToSection);
+    $navLinks.on('click', scrollToSection);
 
 });
