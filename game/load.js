c23cb535791cbdb4027d49358a47e59761f71fdd
dominikmanowski <manowski@protonmail.com>
2018-02-07 10:31:04 +0100

    clouds added

--- game/load.js
+++ game/load.js
@@ -7,6 +7,8 @@ var loadState = {
         game.load.image('groundRight', 'assets/tiles/grassHalfRight.png');
         game.load.image('groundLeft', 'assets/tiles/grassHalfLeft.png');
         game.load.image('gem', 'assets/items/gemBlue.png');
+        game.load.image('cloud-1', 'assets/items/cloud-2.png');
+        game.load.image('cloud-2', 'assets/items/cloud-1.png');
         game.load.spritesheet('dude', 'assets/player/spritesheet.png', 30, 50, 16);
         game.load.image('bgMenu', 'assets/menu/bg_menu.png');
         game.load.image('bgMenuHigh_HowToPlay', 'assets/menu/bg_HighandHowToPlay.png');
--- game/play.js
+++ game/play.js
@@ -1,138 +1,137 @@
 var playState = {
 
+    create: function () {
 
-create: function () {
+        game.add.sprite(0, 0, 'bground');
 
-    game.add.sprite(0, 0, 'bground');
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        platforms = game.add.group();
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+        //  We will enable physics for any object that is created in this group
+        platforms.enableBody = true;
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
+        // Here we create the ground.
 
-    // Here we create the ground.
-
-   function createGround() {
-        for (var i = 0; i <= 800; i += 70) {
-            var ground = platforms.create(i, game.world.height - 124, 'groundCenterFat');
-            ground.body.immovable = true;
+        function createGround() {
+            for (var i = 0; i <= 800; i += 70) {
+                var ground = platforms.create(i, game.world.height - 124, 'groundCenterFat');
+                ground.body.immovable = true;
+            }
+            return ground;
         }
-        return ground;
+
+        createGround();
+
+        function createLedge(x, y, z) {
+            for (var i = 70; i <= 70 * (z - 1); i += 70) {
+                var ledgeCenter = platforms.create(x + i, y, 'groundCenter');
+                ledgeCenter.body.immovable = true;
+            }
+            var ledgeRight = platforms.create(x + i, y, 'groundRight');
+            var ledgeLeft = platforms.create(x, y, 'groundLeft');
+            ledgeRight.body.immovable = true;
+            ledgeLeft.body.immovable = true;
+        }
+
+        createLedge(520, 100, 3);
+        createLedge(0, 100, 4);
+        createLedge(310, 200, 6);
+        createLedge(0, 300, 4);
+        createLedge(600, 350, 3);
+        createLedge(200, 400, 4);
+        createLedge(0, 500, 2);
+        createLedge(520, 500, 3);
+        createLedge(300, 500, 1);
+
+        player = game.add.sprite(300, game.world.height - 200, 'dude');
+
+        game.physics.arcade.enable(player);
+
+        //  Player physics properties. Give the little guy a slight bounce.
+        player.body.bounce.y = 0.1;
+        player.body.gravity.y = 700;
+        player.body.collideWorldBounds = true;
+        player.animations.add('left', [8, 9, 10, 11, 12, 13], 17, true);
+        player.animations.add('right', [2, 3, 4, 5, 6, 7], 17, true);
+        player.animations.add('stop', [14, 15], 3, true);
+        cursors = game.input.keyboard.createCursorKeys();
+
+        var cloud1 = game.add.image(220, 46, 'cloud-1');
+        cloud1.scale.set(.7);
+        cloud1.scale.x *= -1;
+
+        gems = game.add.group();
+
+        gems.enableBody = true;
+
+
+        for (var i = 0; i < 10; i++) {
+            var gem = gems.create(i * 120, 0, 'gem');
+            var gem2 = gems.create(i * 150, 100, 'gem');
+            var gem3 = gems.create(i * 180, 250, 'gem');
+            var gem4 = gems.create(i * 200, 420, 'gem');
+
+            gem.body.gravity.y = 30;
+            gem2.body.gravity.y = 30;
+            gem3.body.gravity.y = 15;
+            gem4.body.gravity.y = 15;
+
+            gem.body.bounce.y = 0.6 + Math.random() * 0.3;
+        }
+
+        var cloud2 = game.add.image(680, 23, 'cloud-1');
+
+    },
+    update: function () {
+        var hitPlatform = game.physics.arcade.collide(player, platforms);
+        var gemHitPlatform = game.physics.arcade.collide(gems, platforms);
+        var playerKillGem = game.physics.arcade.overlap(player, gems, killGem, null, this);
+
+        function killGem(player, gem) {
+            gem.kill();
+        }
+
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown) {
+            //  Move to the left
+            player.body.velocity.x = -150;
+
+            player.animations.play('left');
+        }
+        else if (cursors.right.isDown) {
+            //  Move to the right
+            player.body.velocity.x = 150;
+
+            player.animations.play('right');
+        }
+        else {
+            //  Stand still
+            player.animations.play('stop');
+        }
+
+        if (cursors.up.isDown && cursors.right.isDown && player.body.touching.down) {
+            player.body.velocity.y = -400;
+            player.animations.stop();
+            player.frame = 0;
+        }
+        if (cursors.up.isDown && cursors.right.isDown) {
+            player.animations.stop();
+            player.frame = 0;
+        }
+        if (cursors.up.isDown && cursors.left.isDown && player.body.touching.down) {
+            player.body.velocity.y = -400;
+            player.animations.stop();
+            player.frame = 1;
+        }
+        if (cursors.up.isDown && cursors.left.isDown) {
+            player.animations.stop();
+            player.frame = 1;
+        }
+        if (cursors.up.isDown && player.body.touching.down) {
+            player.body.velocity.y = -400;
+            player.animations.stop();
+            player.frame = 0;
+        }
     }
-    createGround();
-
-   function createLedge(x, y, z) {
-       for (var i = 70; i <= 70*(z-1); i += 70) {
-           var ledgeCenter = platforms.create(x+i, y, 'groundCenter');
-           ledgeCenter.body.immovable = true;
-       }
-       var ledgeRight = platforms.create(x+i, y, 'groundRight');
-       var ledgeLeft = platforms.create(x, y, 'groundLeft');
-       ledgeRight.body.immovable = true;
-       ledgeLeft.body.immovable = true;
-   }
-    createLedge(520,100,3);
-    createLedge(0,100,4);
-    createLedge(310,200,6);
-    createLedge(0,300,4);
-    createLedge(600,350,3);
-    createLedge(200,400,4);
-    createLedge(0,500,2);
-    createLedge(520,500,3);
-    createLedge(300,500,1);
-
-    player = game.add.sprite(300, game.world.height - 200, 'dude');
-
-    game.physics.arcade.enable(player);
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.1;
-    player.body.gravity.y = 700;
-    player.body.collideWorldBounds = true;
-    player.animations.add('left', [8, 9, 10, 11, 12, 13], 17, true);
-    player.animations.add('right', [2, 3, 4, 5, 6, 7], 17, true);
-    player.animations.add('stop', [14, 15], 3, true);
-    cursors = game.input.keyboard.createCursorKeys();
-
-
-    gems = game.add.group();
-
-    gems.enableBody = true;
-
-    for (var i = 0; i < 10; i++)
-    {
-        var gem = gems.create(i * 120, 0, 'gem');
-        var gem2 = gems.create(i * 150, 100, 'gem');
-        var gem3 = gems.create(i * 180, 250, 'gem');
-        var gem4 = gems.create(i * 200, 420, 'gem');
-
-        gem.body.gravity.y = 30;
-        gem2.body.gravity.y = 30;
-        gem3.body.gravity.y = 15;
-        gem4.body.gravity.y = 15;
-
-        gem.body.bounce.y = 0.6 + Math.random() * 0.3;
-    }
-
-},
- update: function() {
-    var hitPlatform = game.physics.arcade.collide(player, platforms);
-    var gemHitPlatform = game.physics.arcade.collide(gems, platforms);
-    var playerKillGem = game.physics.arcade.overlap(player, gems, killGem, null, this);
-
-    function killGem (player, gem) {
-        gem.kill();
-    }
-
-    player.body.velocity.x = 0;
-
-    if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.x = -150;
-
-        player.animations.play('left');
-    }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.x = 150;
-
-        player.animations.play('right');
-    }
-    else
-    {
-        //  Stand still
-        player.animations.play('stop');
-    }
-
-    if (cursors.up.isDown && cursors.right.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -400;
-        player.animations.stop();
-        player.frame = 0;
-    }
-    if (cursors.up.isDown && cursors.right.isDown)
-    {
-        player.animations.stop();
-        player.frame = 0;
-    }
-    if (cursors.up.isDown && cursors.left.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -400;
-        player.animations.stop();
-        player.frame = 1;
-    }
-    if (cursors.up.isDown && cursors.left.isDown)
-    {
-        player.animations.stop();
-        player.frame = 1;
-    }
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -400;
-        player.animations.stop();
-        player.frame = 0;
-    }
-}};
+};
